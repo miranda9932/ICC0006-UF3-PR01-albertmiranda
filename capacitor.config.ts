2e46eaca1129d842a06dcb270bf92f7c53c68bb0
miranda9932 <albertmirandabayes@gmail.com>
2025-05-08 12:30:32 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ICC0006-UF3-PR01-albertmiranda',
+  webDir: 'www'
+};
+
+export default config;
